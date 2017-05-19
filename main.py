8a114ba23e222fa4d9d6cc0f2d1a96c0580f5648
Igor Mróz <mrozigor@wp.pl>
2017-05-19 21:21:01 +0200

    Initial commit.

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+#from curses import wrapper
+
+#def main(standardScreen):
+#    currentKey = standardScreen.getkey()
+#    while currentKey != 'q':
+#        standardScreen.clear()
+#
+#        standardScreen.addstr(0,0,'TEST')
+#
+#        standardScreen.refresh()
+#        currentKey = standardScreen.getkey()
+#
+#wrapper(main)
